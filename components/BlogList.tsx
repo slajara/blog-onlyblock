c83b3d9d0d2a419672668300192c5aa17849d01e
slajara <lajara.agullo@gmail.com>
2024-06-20 15:46:23 +0100

    add article page and description

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -2,6 +2,7 @@ import urlFor from "@/sanity/lib/image";
 import { Post } from "@/typings";
 import { ArrowUpRightIcon } from "@heroicons/react/16/solid";
 import Image from "next/image";
+import ClientSideRoute from "./ClientSideRoute";
 
 type Props = {
   posts: Post[];
@@ -14,50 +15,54 @@ function BlogList({ posts }: Props) {
   return (
     <div>
       <hr className="border-[#F7Ab0A] mb-10" />
+
       <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
         {posts.map((post) => (
-          <div key={post._id} className="flex flex-col group cursor-pointer">
-            <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
-              <Image
-                className="object-cover object-left lg:object-center"
-                src={urlFor(post.mainImage).url()}
-                alt={post._createdAt}
-                fill
-                priority={true}
-              />
-              <div className="absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between">
-                <div>
-                  <p className="font-bold">{post.title} </p>
-                  <p>
-                    {new Date(post._createdAt).toLocaleDateString("en-US", {
-                      day: "numeric",
-                      month: "long",
-                      year: "numeric",
-                    })}
-                  </p>
-                </div>
-                <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center">
-                  {post.categories?.map((category) => (
-                    <div
-                      key={post._id}
-                      className="bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold"
-                    >
-                      <p>{category.title}</p>
-                    </div>
-                  ))}
+          <ClientSideRoute key={post._id} route={`/post/${post.slug?.current}`}>
+            <div className="flex flex-col group cursor-pointer">
+              <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
+                <Image
+                  className="object-cover object-left lg:object-center"
+                  src={urlFor(post.mainImage).url()}
+                  alt={post._createdAt}
+                  fill
+                  priority={true}
+                
+                />
+                <div className="absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between">
+                  <div>
+                    <p className="font-bold">{post.title} </p>
+                    <p>
+                      {new Date(post._createdAt).toLocaleDateString("en-US", {
+                        day: "numeric",
+                        month: "long",
+                        year: "numeric",
+                      })}
+                    </p>
+                  </div>
+                  <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center">
+                    {post.categories?.map((category) => (
+                      <div
+                        key={post._id}
+                        className="bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold"
+                      >
+                        <p>{category.title}</p>
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="mt-5 flex-1">
-              <p className="underline text-lg font-blod"> {post.title}</p>
-              <p className="line-clamp-2 text-gray-500">{post.description}</p>
-            </div>
+              <div className="mt-5 flex-1">
+                <p className="underline text-lg font-blod"> {post.title}</p>
+                <p className="line-clamp-2 text-gray-500">{post.description}</p>
+              </div>
 
-            <p className="mt-5 font-bold flex items-center group-hover:underline">
-              Read Post
-              <ArrowUpRightIcon className="ml-2 h-4 w-4" />
-            </p>
-          </div>
+              <p className="mt-5 font-bold flex items-center group-hover:underline">
+                Read Post
+                <ArrowUpRightIcon className="ml-2 h-4 w-4" />
+              </p>
+            </div>
+          </ClientSideRoute>
         ))}
       </div>
     </div>
