97e5505bafc486f2b1f0d541a2f6d9b9968957a1
slajara <lajara.agullo@gmail.com>
2024-06-22 19:38:26 +0100

    update package.json

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,34 +1,22 @@
-'use client'
+"use client";
 
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...tool]]/page.tsx` route
- */
-
-import {visionTool} from '@sanity/vision'
-import {Studio, StudioNavbar, defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './sanity/env'
-import {schema} from './sanity/schema'
+import { visionTool } from "@sanity/vision";
+import { StudioNavbar, defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { apiVersion, dataset, projectId } from "./sanity/env";
+import { schema } from "./sanity/schema";
 
 export default defineConfig({
-  basePath: '/studio',
+  basePath: "/studio",
   name: "Slajara_Studio",
   title: "Slajara Studio",
   projectId,
   dataset,
-  // Add and edit the content schema in the './sanity/schema' folder
   schema,
-  plugins: [
-    structureTool(),
-    // Vision is a tool that lets you query your content with GROQ in the studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
-  ],
-  studio:{
-    components:{
+  plugins: [structureTool(), visionTool({ defaultApiVersion: apiVersion })],
+  studio: {
+    components: {
       navbar: StudioNavbar,
     },
   },
-})
+});
